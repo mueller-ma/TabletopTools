3be00026d4933f8f5c48807b84bdb0fe9dd5fd2c
mueller-ma <mueller-ma@users.noreply.github.com>
2021-05-12 23:38:00 +0200

    Remove unused resources

--- app/src/main/java/com/github/muellerma/tabletoptools/MainActivity.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/MainActivity.kt
@@ -36,12 +36,6 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
