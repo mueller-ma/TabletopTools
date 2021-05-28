f0908f616be1ff0e4e7ff582841712e30d793e93
mueller-ma <mueller-ma@users.noreply.github.com>
2021-05-28 10:59:44 +0200

    Vibrate on buzzer press

--- app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/AbstractBaseFragment.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/AbstractBaseFragment.kt
@@ -3,8 +3,13 @@ package com.github.muellerma.tabletoptools.ui.fragments
 import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
-abstract class AbstractBaseFragment : Fragment() {
+abstract class AbstractBaseFragment : Fragment(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + job
     var savedData: SavedData? = null
 }
 
--- app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/BuzzersFragment.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/BuzzersFragment.kt
@@ -1,24 +1,18 @@
 package com.github.muellerma.tabletoptools.ui.fragments
 
-import android.annotation.SuppressLint
-import android.content.pm.ActivityInfo
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.os.*
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.getSystemService
 import com.github.muellerma.tabletoptools.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 
 class BuzzersFragment : AbstractBaseFragment() {
-    private lateinit var buzzerA: Button
-    private lateinit var buzzerB: Button
+    private val buzzers = ArrayList<Button>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,22 +21,44 @@ class BuzzersFragment : AbstractBaseFragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_buzzers, container, false)
 
-        buzzerA = root.findViewById(R.id.buzzer_a)
-        buzzerB = root.findViewById(R.id.buzzer_b)
-
-        buzzerA.setOnClickListener { temporarilyDisableButton(buzzerB) }
-        buzzerB.setOnClickListener { temporarilyDisableButton(buzzerA) }
+        listOf(R.id.buzzer_a, R.id.buzzer_b).forEach { id ->
+            root.findViewById<Button>(id)?.apply {
+                buzzers.add(this)
+                setOnClickListener { buttonPressed(this) }
+            }
+        }
 
         return root
     }
 
-    private fun temporarilyDisableButton(button: Button) {
-        Log.d(TAG, "Disable button ${button.text}")
-        button.isEnabled = false
-        GlobalScope.launch {
+    private fun buttonPressed(pressedButton: Button) {
+        Log.d(TAG, "Pressed button ${pressedButton.text}")
+
+        val vibrator = requireContext().getSystemService<Vibrator>()!!
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            vibrator.vibrate(500)
+        }
+
+        buzzers.forEach {
+            if (it != pressedButton) {
+                it.isEnabled = false
+            }
+        }
+
+        launch {
             delay(5 * 1000)
             Handler(Looper.getMainLooper()).post {
-                button.isEnabled = true
+                buzzers.forEach {
+                    if (it != pressedButton) {
+                        it.isEnabled = true
+                    }
+                }
             }
         }
     }
