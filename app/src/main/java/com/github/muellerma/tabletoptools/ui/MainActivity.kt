2c9f13cc0ab6783e97d6b5a5673a264ced4f364b
Benjamin Kadel <kardeliogithub@outlook.com>
2022-10-06 16:31:43 +0200

    Added ability to select default tab (#166)
    Fixes #152

--- app/src/main/java/com/github/muellerma/tabletoptools/ui/MainActivity.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.github.muellerma.tabletoptools.R
 import com.github.muellerma.tabletoptools.databinding.ActivityMainBinding
 import com.github.muellerma.tabletoptools.ui.fragments.AbstractBaseFragment
+import com.github.muellerma.tabletoptools.utils.Prefs
 import com.github.muellerma.tabletoptools.utils.parcelable
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBar.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
+
+        val navGraph = navController.navInflater.inflate(R.navigation.mobile_navigation)
+        navGraph.setStartDestination(getDefaultStartDestinationId())
+        navController.graph = navGraph
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -43,6 +49,19 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+    }
+
+    private fun getDefaultStartDestinationId(): Int {
+        return when (Prefs(this).startPage) {
+            getString(R.string.menu_dices_value) -> R.id.nav_dices
+            getString(R.string.menu_random_list_value) -> R.id.nav_random_list
+            getString(R.string.menu_rot13_value) -> R.id.nav_rot13
+            getString(R.string.menu_alphabet_value) -> R.id.nav_alphabet
+            getString(R.string.menu_number_converter_value) -> R.id.nav_number_converter
+            getString(R.string.menu_timer_value) -> R.id.nav_timer
+            getString(R.string.menu_buzzers_value) -> R.id.nav_buzzers
+            else -> R.id.nav_dices
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
