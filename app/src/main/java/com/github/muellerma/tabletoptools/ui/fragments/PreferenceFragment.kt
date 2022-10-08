91055f1611e8d8871302f1b4ce01e79c50209df6
Iman Irandoost <imn.irdst@gmail.com>
2022-10-08 13:27:11 +0200

    Add about page

--- app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/PreferenceFragment.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/PreferenceFragment.kt
@@ -10,20 +10,93 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.core.view.setPadding
+import androidx.fragment.app.commit
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.github.muellerma.tabletoptools.R
+import com.github.muellerma.tabletoptools.utils.openInBrowser
 import com.github.muellerma.tabletoptools.utils.positionInAlphabet
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.slider.Slider
+import com.mikepenz.aboutlibraries.LibsBuilder
+import com.mikepenz.aboutlibraries.LibsConfiguration
+import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.util.SpecialButton
 import kotlinx.parcelize.Parcelize
 import org.w3c.dom.Text
 
 class PreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.pref_main)
+
+        getPreference("about").apply {
+            setOnPreferenceClickListener {
+                val fragment = LibsBuilder()
+                    .withActivityTitle(getString(R.string.about))
+                    .withAboutIconShown(true)
+                    .withAboutVersionShownName(true)
+                    .withSortEnabled(true)
+                    .withListener(AboutButtonsListener())
+                    .supportFragment()
+
+                parentFragmentManager.commit {
+                    addToBackStack("about")
+                    replace(R.id.nav_host_fragment, fragment, "about")
+                }
+                true
+            }
+        }
     }
 
     companion object {
         private var TAG = PreferenceFragment::class.java.simpleName
+    }
+}
+
+fun PreferenceFragmentCompat.getPreference(key: String) =
+    preferenceManager.findPreference<Preference>(key)!!
+
+
+class AboutButtonsListener : LibsConfiguration.LibsListener {
+    override fun onExtraClicked(v: View, specialButton: SpecialButton): Boolean {
+        val link = when (specialButton) {
+            SpecialButton.SPECIAL1 -> "https://github.com/mueller-ma/TabletopTools/"
+            SpecialButton.SPECIAL2 -> "https://f-droid.org/packages/com.github.muellerma.tabletoptools/"
+            SpecialButton.SPECIAL3 -> "https://crowdin.com/project/tabletop-tools"
+        }
+        link.openInBrowser(v.context)
+        return true
+    }
+
+    override fun onIconClicked(v: View) {
+        // no-op
+    }
+
+    override fun onIconLongClicked(v: View): Boolean {
+        return false
+    }
+
+    override fun onLibraryAuthorClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryAuthorLongClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryBottomClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryBottomLongClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryContentClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryContentLongClicked(v: View, library: Library): Boolean {
+        return false
     }
 }
--- app/src/main/java/com/github/muellerma/tabletoptools/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/utils/ExtensionFunctions.kt
@@ -1,9 +1,35 @@
 package com.github.muellerma.tabletoptools.utils
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.StringRes
+import com.github.muellerma.tabletoptools.R
+
+private const val TAG = "ExtensionFunctions"
 
 inline fun <reified T> Bundle.parcelable(key: String): T? = when {
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
+
+fun String.openInBrowser(context: Context) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(this))
+    try {
+        context.startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        Log.d(TAG, "Unable to open url in browser: $intent")
+        context.showToast(R.string.error_no_browser_found)
+    }
+}
+
+fun Context.showToast(@StringRes msg: Int) {
+    Toast
+        .makeText(this, msg, Toast.LENGTH_SHORT)
+        .show()
 }
