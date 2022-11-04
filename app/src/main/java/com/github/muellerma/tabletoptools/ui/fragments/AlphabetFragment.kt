bdbd8847454a7aeb23e517a8f76ac9fded1d450e
Akito <the@akito.ooo>
2022-11-05 00:15:27 +0100

    Add Keep Screen On to Alphabet

--- app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/AlphabetFragment.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/AlphabetFragment.kt
@@ -2,30 +2,30 @@ package com.github.muellerma.tabletoptools.ui.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
-import androidx.core.view.setPadding
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.github.muellerma.tabletoptools.R
 import com.github.muellerma.tabletoptools.databinding.AlphabetTableRowBinding
 import com.github.muellerma.tabletoptools.databinding.FragmentAlphabetBinding
+import com.github.muellerma.tabletoptools.utils.Prefs
 import com.github.muellerma.tabletoptools.utils.positionInAlphabet
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.slider.Slider
-import kotlinx.parcelize.Parcelize
-import org.w3c.dom.Text
 
 class AlphabetFragment : AbstractBaseFragment() {
+    private lateinit var prefs: Prefs
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentAlphabetBinding.inflate(inflater, container, false)
+        val alphabet = binding.alphabetScrollView
 
         ('A'..'Z').forEach { letter ->
             @SuppressLint("InflateParams") // View is set as child below
@@ -37,6 +37,28 @@ class AlphabetFragment : AbstractBaseFragment() {
 
             binding.alphabetTable.addView(row.root)
         }
+
+        prefs = Prefs(alphabet.context)
+
+        alphabet.keepScreenOn = prefs.alphabetKeepScreenOn
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_alphabet, menu)
+            }
+
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                return when (item.itemId) {
+                    R.id.keep_screen_on -> {
+                        val nowKeepScreenOn = alphabet.keepScreenOn.not()
+                        prefs.alphabetKeepScreenOn = nowKeepScreenOn
+                        alphabet.keepScreenOn = nowKeepScreenOn
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         return binding.root
     }
