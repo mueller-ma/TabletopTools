02bcd13d558144ed1f7252df734cad3db7b0509a
Akito <the@akito.ooo>
2022-11-05 00:32:06 +0100

    Add Keep Screen On to Number Converter

--- app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/NumberConverterFragment.kt
+++ app/src/main/java/com/github/muellerma/tabletoptools/ui/fragments/NumberConverterFragment.kt
@@ -3,16 +3,21 @@ package com.github.muellerma.tabletoptools.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import androidx.core.view.MenuProvider
 import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.github.muellerma.tabletoptools.R
 import com.github.muellerma.tabletoptools.databinding.FragmentNumberConverterBinding
-import java.util.*
+import com.github.muellerma.tabletoptools.utils.Prefs
+import java.util.Locale
 
 class NumberConverterFragment : AbstractBaseFragment() {
+    private lateinit var prefs: Prefs
     private var inputFromUser = true
 
     override fun onCreateView(
@@ -21,6 +26,7 @@ class NumberConverterFragment : AbstractBaseFragment() {
             savedInstanceState: Bundle?
     ): View {
         val binding = FragmentNumberConverterBinding.inflate(inflater, container, false)
+        val numberConverter = binding.numberConverterScrollView
         val binInputText = binding.numberConverterInputBin
         val decInputText = binding.numberConverterInputDec
         val hexInputText = binding.numberConverterInputHex
@@ -63,6 +69,29 @@ class NumberConverterFragment : AbstractBaseFragment() {
             binInputText.setText(Integer.toBinaryString(input))
             inputFromUser = true
         }
+
+        prefs = Prefs(numberConverter.context)
+
+        numberConverter.keepScreenOn = prefs.numberConverterKeepScreenOn
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_number_converter, menu)
+            }
+
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                return when (item.itemId) {
+                    R.id.keep_screen_on -> {
+                        val nowKeepScreenOn = numberConverter.keepScreenOn.not()
+                        prefs.numberConverterKeepScreenOn = nowKeepScreenOn
+                        numberConverter.keepScreenOn = nowKeepScreenOn
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
     }
 
